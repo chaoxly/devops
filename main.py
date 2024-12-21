742545b8f885908e38ca641aa4e8e303aa0ee9c7
Michal Kwak <115922717+chaoxly@users.noreply.github.com>
2024-12-21 21:14:38 +0000

    Dodano Trivy

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Routing dla localhost:5000/michal
 @app.route('/michal')
 def michal():
-    return "Witaj Michal"
+    return "Witaj XYZ"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
