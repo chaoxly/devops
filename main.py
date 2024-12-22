6a0d39d7ca41e85867308099cf4c4935eafaccb0
Michal Kwak <115922717+chaoxly@users.noreply.github.com>
2024-12-22 16:26:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Routing dla localhost:5000/michal
 @app.route('/michal')
 def michal():
-    return "Witaj XYZ"
+    return "Witaj Michal"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
